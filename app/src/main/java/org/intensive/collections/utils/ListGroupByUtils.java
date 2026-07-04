2828beec56da475beadb271db29bc2201861e6cd
Andrii Shtanko <andrii.shtanko@pitchbook.com>
2026-07-04 12:56:32 +0300

    HW4

--- app/src/main/java/org/intensive/collections/utils/ListGroupByUtils.java
+++ app/src/main/java/org/intensive/collections/utils/ListGroupByUtils.java
@@ -1,6 +1,10 @@
 package org.intensive.collections.utils;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 
 
