3824d34e2749837dcf17c946ab82171c21a0c7eb
Andrii Shtanko <andrii.shtanko@pitchbook.com>
2026-07-14 10:55:07 +0300

    Add declared order of locks

--- app/src/main/java/org/intensive/threads/BankAccount.java
+++ app/src/main/java/org/intensive/threads/BankAccount.java
@@ -23,6 +23,8 @@ public class BankAccount  {
     private boolean deposit(double amount) throws InterruptedException {
         if (lock.tryLock()) {
             try {
+                Thread.sleep(1000);
+
                 balance += amount;
                 return true;
             } finally {
@@ -37,6 +39,7 @@ public class BankAccount  {
     private boolean withdraw(double amount) throws InterruptedException {
         if (lock.tryLock()) {
             try {
+                Thread.sleep(1000);
                 balance -= amount;
                 return true;
             } finally {
@@ -49,28 +52,40 @@ public class BankAccount  {
     }
 
     public void transfer (BankAccount to, double amount) throws InterruptedException {
+        BankAccount firstLock;
+        BankAccount secondLock;
+
+        if (this.accountNumber.compareTo(to.accountNumber) < 0) {
+            firstLock = this;
+            secondLock = to;
+        }else {
+            firstLock = to;
+            secondLock = this;
+        }
+
+
         boolean transferSuccess = false;
 
         while (!transferSuccess) {
-            if (withdraw(amount)) {
+            if (firstLock.withdraw(amount)) {
                 if (to.deposit(amount)) {
                     System.out.println(Thread.currentThread().getName() + ": transfer " + amount);
                     transferSuccess = true;
                 } else {
-                    System.out.println(Thread.currentThread().getName() + " Destination account is busy. Trying to refund money...");
+                    System.out.println(Thread.currentThread().getName() + " Destination account is busy.");
                      boolean refundSuccess = false;
                     while (!refundSuccess) {
-                        if (deposit(amount)) {
+                        if (firstLock.deposit(amount)) {
                             refundSuccess = true;
                         } else {
-                            Thread.sleep(Math.toIntExact(Math.round(Math.random() * 10)));
+                            Thread.sleep(Math.toIntExact(Math.round(Math.random() * 1000)));
                         }
                     }
 
                 }
             }else {
                 System.out.println(Thread.currentThread().getName() + " Destination account is busy.");
-                Thread.sleep(Math.toIntExact(Math.round(Math.random() * 10)));
+                Thread.sleep(Math.toIntExact(Math.round(Math.random() * 1000)));
             }
         }
         System.out.println(Thread.currentThread().getName() + ": Transfer successful");
